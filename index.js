944cc22a55a85b073d014d3ef09f61a0f90fb4cb
unknown <hunchographics@gmail.com>
2023-02-24 16:26:54 +0300

    pld

--- index.js
+++ index.js
@@ -88,7 +88,6 @@ exports.getMintScan = async () => {
   // Go to your site
   const addressMint = url.split("/")[5];
   await page.goto('https://www.mintscan.io/cosmos/account/cosmos1dy6ndu0wc5n29lfkw5gh6zpvlh2vf0u8ug8lae', { waitUntil: "networkidle0", timeout: 0 });
-  
   await delay(10000);
   const tokenMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_totalValueWrapper__2Da_d > div.AccountInfo_totalValue__E0ehd > span:nth-child(1)", el => el.innerText);
   console.log(tokenMint);
