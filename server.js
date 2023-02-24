e4612d0a2524d70d27f4b859ea1b7c24eaedeebd
unknown <hunchographics@gmail.com>
2023-02-24 16:04:41 +0300

    please wor aain

--- server.js
+++ server.js
@@ -32,23 +32,7 @@ app.use('/api/senddata', (req, res) => {
 
   Promise.all([thorChain, mintScan, debank]).then((values) => {
     console.log(values);
-    // var html = 
-    // `<!DOCTYPE html>
-    // <html lang="en">
-    //  <head>
-    //  <meta charset="UTF-8">
-    //  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    //  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    //  </head>
-    // <body style="background-color:#111827; color:white;">
-    // <h1>node server </h1>
-    // <div style="display:flex; height: 70%;flex-direction:column; justify-content:between; margin:50px,auto;">
-    //   ${values[0]}
-    //   ${values[1]}
-    //   ${values[2]}
-    //   <div>
-    // </body>
-    // </html>`
+
     res.send({data: values});
 
    
