3e797a687ff5b09c9c1e442c87a3ed6d0f9cb76c
unknown <hunchographics@gmail.com>
2023-02-24 18:58:18 +0300

    final

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ exports.getMintScan = async () => {
   const url = "https://www.mintscan.io/cosmos/account/cosmos1dy6ndu0wc5n29lfkw5gh6zpvlh2vf0u8ug8lae";
   // Go to your site
   const addressMint = url.split("/")[5];
-  await page.goto('https://www.mintscan.io/cosmos/account/cosmos1dy6ndu0wc5n29lfkw5gh6zpvlh2vf0u8ug8lae', { waitUntil: "domcontentloaded", timeout: 0 });
+  await page.goto('https://www.mintscan.io/cosmos/account/cosmos1dy6ndu0wc5n29lfkw5gh6zpvlh2vf0u8ug8lae', { waitUntil: "networkidle0", timeout: 0 });
 
   const tokenMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_totalValueWrapper__2Da_d > div.AccountInfo_totalValue__E0ehd > span:nth-child(1)", el => el.innerText);
   await delay(10000);
