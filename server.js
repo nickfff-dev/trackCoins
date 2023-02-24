aa6906361df86e88acc1d9bb2e7a4443941d1b44
unknown <hunchographics@gmail.com>
2023-02-24 22:27:21 +0300

    ps2

--- server.js
+++ server.js
@@ -10,35 +10,31 @@ const { getThorChain } = require('./index')
 
 
 app.use(express.static(__dirname + '/public'))
+const debank = new Promise((resolve, reject) => {
+  getDebank()
+    .then(data => {
+      resolve(data)
+    })
+    .catch(err => reject('debank scrape failed'))
+})
 
+const mintScan = new Promise((resolve, reject) => {
+  getMintScan()
+    .then(data => {
+      resolve(data)
+    })
+    .catch(err => reject('mintScan scrape failed'))
+})
+
+const thorChain = new Promise((resolve, reject) => {
+  getThorChain()
+    .then(data => {
+      resolve(data)
+    })
+    .catch(err => reject('thorChain scrape failed'))
+})
 
 app.use('/api/senddata', (req, res) => {
-  const debank = new Promise((resolve, reject) => {
-    getDebank()
-      .then(data => {
-        resolve(data)
-      })
-      .catch(err => reject('debank scrape failed'))
-  })
-
-  const mintScan = new Promise((resolve, reject) => {
-    getMintScan()
-      .then(data => {
-        resolve(data)
-      })
-      .catch(err => reject('mintScan scrape failed'))
-  })
-
-  const thorChain = new Promise((resolve, reject) => {
-    getThorChain()
-      .then(data => {
-        resolve(data)
-      })
-      .catch(err => reject('thorChain scrape failed'))
-  })
-
-
-  
   Promise.all([thorChain, mintScan, debank]).then((values) => {
     console.log(values);
 
