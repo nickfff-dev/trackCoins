a900f7c2298f0e61be5fbf6b8c3af7c96b0faa2c
unknown <hunchographics@gmail.com>
2023-02-25 23:26:40 +0300

    wh

--- dataroute.js
+++ dataroute.js
@@ -13,20 +13,16 @@ datarouter.get('/api/getmintscan', (req, res) => {
   const page = await browser.newPage();
   
   const url = "https://www.mintscan.io/cosmos/account/cosmos1dy6ndu0wc5n29lfkw5gh6zpvlh2vf0u8ug8lae";
-  await page.goto(url, { waitUntil: "networkidle2", timeout:0})
-  console.log(await page.content())
-  const lastRes = await page.waitForResponse("https://lcd-cosmos.cosmostation.io/cosmos/bank/v1beta1/balances/cosmos1dy6ndu0wc5n29lfkw5gh6zpvlh2vf0u8ug8lae?pagination.limit=1000" , this.options = {
-    timeout:0
-  });
-  console.log(lastRes)
-  if (lastRes.ok() && lastRes.status() === 200) {
+  await page.goto(url, { waitUntil: "domcontentloaded"})
+  await Promise.all([page.goto(url, { waitUntil: "domcontentloaded"}), page.waitForResponse("https://lcd-cosmos.cosmostation.io/cosmos/bank/v1beta1/balances/cosmos1dy6ndu0wc5n29lfkw5gh6zpvlh2vf0u8ug8lae?pagination.limit=1000")])
+
    
     const addressMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_address__2WY10.AccountInfo_cursor__1Nv86", el => el.innerText);
     const tokenMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_totalValueWrapper__2Da_d > div.AccountInfo_totalValue__E0ehd > span:nth-child(1)",  (el) => el.innerText);
     const values = [{addressMint, tokenMint}]
     console.log(values)
     res.send({ data: values });
-  }
+  
 
  
 })()
