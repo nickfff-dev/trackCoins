d466d60ab41bef0a1226a1e44664ae853bc6b120
unknown <hunchographics@gmail.com>
2023-02-26 00:11:20 +0300

    wdd

--- dataroute.js
+++ dataroute.js
@@ -18,17 +18,18 @@ datarouter.get('/api/getmintscan', (req, res) => {
   console.log("waited")
   var values = []
   page.waitForXPath("/html/body/div/main/section/div/div[3]/section[2]/div/div[2]/div", {
+    visible:true,
     timeout:0
-  }).then((data) => {
-
+  }).then(async (data) => {
+    const addressMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_address__2WY10.AccountInfo_cursor__1Nv86", el => el.innerText);
+    values.push(addressMint)
+    const tokenMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_totalValueWrapper__2Da_d > div.AccountInfo_totalValue__E0ehd", (el) => el.innerText);
+    values.push(tokenMint)
+    console.log({addressMint,tokenMint})
    console.log("foff", data)
   })
   
-  const addressMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_address__2WY10.AccountInfo_cursor__1Nv86", el => el.innerText);
-  values.push(addressMint)
-  const tokenMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_totalValueWrapper__2Da_d > div.AccountInfo_totalValue__E0ehd", (el) => el.innerText);
-  values.push(tokenMint)
-  console.log({addressMint,tokenMint})
+
 
    
     
