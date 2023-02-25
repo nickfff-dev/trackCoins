a9707b2f74de555feaf61e034381ec5da26fab0a
unknown <hunchographics@gmail.com>
2023-02-25 22:27:09 +0300

    alsge

--- dataroute.js
+++ dataroute.js
@@ -11,24 +11,19 @@ datarouter.get('/api/getmintscan', (req, res) => {
 (async () => {
   browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
   const page = await browser.newPage();
-  var values = []
+  
   const url = "https://www.mintscan.io/cosmos/account/cosmos1dy6ndu0wc5n29lfkw5gh6zpvlh2vf0u8ug8lae";
-  Promise.all([page.goto(url, { waitUntil: "domcontentloaded", timeout:0 }), new Promise(function (resolve) { setTimeout(resolve, 60000) })]).then( async () => {
+  await page.goto(url, { waitUntil: "networkidle2", timeout:0})
+  
+  const lastReq = await page.waitForRequest((request) => { request.url() === "https://lcd-cosmos.cosmostation.io/cosmos/bank/v1beta1/balances/cosmos1dy6ndu0wc5n29lfkw5gh6zpvlh2vf0u8ug8lae?pagination.limit=1000" });
+  if (lastReq.response() && lastReq.response()?.ok()) {
     const addressMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_address__2WY10.AccountInfo_cursor__1Nv86", el => el.innerText);
     const tokenMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_totalValueWrapper__2Da_d > div.AccountInfo_totalValue__E0ehd > span:nth-child(1)",  (el) => el.innerText);
-    values.push(addressMint)
-    values.push(tokenMint)
+    const values = [{addressMint, tokenMint}]
     console.log(values)
-   
-  })
-
-
-  if (values.length > 1) {
-    res.send({data:values})
+    res.send({ data: values });
   }
-  
 
- 
  
 })()
   .catch(err => console.error(err))
