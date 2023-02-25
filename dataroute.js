d9cac8d27b8b421a54c63202491e69ac0689e02e
unknown <hunchographics@gmail.com>
2023-02-25 21:41:40 +0300

    timestamp

--- dataroute.js
+++ dataroute.js
@@ -10,10 +10,10 @@ let browser;
 datarouter.get('/api/getmintscan', (req, res) => {
 (async () => {
   browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
-  const [page] = await browser.pages();
+  const [page] = await browser.newPage();
   
   const url = "https://www.mintscan.io/cosmos/account/cosmos1dy6ndu0wc5n29lfkw5gh6zpvlh2vf0u8ug8lae";
-  await page.goto(url,  { waitUntil: "networkidle0",timeout:60000});
+  await Promise.all([page.goto(url, { waitUntil: "domcontentloaded" }), page.waitForNetworkIdle({ idleTime: 60000 })]);
   const addressMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_address__2WY10.AccountInfo_cursor__1Nv86", el => el.innerText);
   const tokenMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_totalValueWrapper__2Da_d > div.AccountInfo_totalValue__E0ehd > span:nth-child(1)",  (el) => el.innerText);
   const values = [{addressMint, tokenMint}]
