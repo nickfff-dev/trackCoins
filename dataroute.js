07f3f9dc147f07010f34e7a84dcc0e9f483757c8
unknown <hunchographics@gmail.com>
2023-02-25 23:36:40 +0300

    wh

--- dataroute.js
+++ dataroute.js
@@ -13,8 +13,8 @@ datarouter.get('/api/getmintscan', (req, res) => {
   const page = await browser.newPage();
   
   const url = "https://www.mintscan.io/cosmos/account/cosmos1dy6ndu0wc5n29lfkw5gh6zpvlh2vf0u8ug8lae";
-  await page.goto(url, { waitUntil: "domcontentloaded"})
-  await Promise.all([page.goto(url, { waitUntil: "networkidle0"}), page.waitForRequest("https://api.mintscan.io/v1/cosmos/validators")])
+
+  Promise.all([page.goto(url, { waitUntil: "networkidle0"}), page.waitForRequest("https://api.mintscan.io/v1/cosmos/validators")])
 
    
     const addressMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_address__2WY10.AccountInfo_cursor__1Nv86", el => el.innerText);
