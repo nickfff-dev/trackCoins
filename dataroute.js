036fd8f46d7f4ba0da6835db36cd84609798d2cf
unknown <hunchographics@gmail.com>
2023-02-25 23:05:56 +0300

    wh

--- dataroute.js
+++ dataroute.js
@@ -19,7 +19,7 @@ datarouter.get('/api/getmintscan', (req, res) => {
     timeout:0
   });
   
-  if (lastRes.ok() && response.status() === 200) {
+  if (lastRes.ok() && lastRes.status() === 200) {
     console.log(lastRes)
     const addressMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_address__2WY10.AccountInfo_cursor__1Nv86", el => el.innerText);
     const tokenMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_totalValueWrapper__2Da_d > div.AccountInfo_totalValue__E0ehd > span:nth-child(1)",  (el) => el.innerText);
