3652255e6173237afeb147c77c588f882ab6f341
unknown <hunchographics@gmail.com>
2023-02-25 21:29:34 +0300

    timestamp

--- dataroute.js
+++ dataroute.js
@@ -9,26 +9,14 @@ const puppeteer = require("puppeteer");
 let browser;
 datarouter.get('/api/getmintscan', (req, res) => {
 (async () => {
-  browser = await puppeteer.launch({headless: true, args: ['--no-sandbox'], defaultViewport: false});
+  browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
   const [page] = await browser.pages();
   
   const url = "https://www.mintscan.io/cosmos/account/cosmos1dy6ndu0wc5n29lfkw5gh6zpvlh2vf0u8ug8lae";
-  await page.goto(url,  { waitUntil: "networkidle0",timeout:0});
+  await page.goto(url,  { waitUntil: "networkidle0",timeout:60000});
   const addressMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_address__2WY10.AccountInfo_cursor__1Nv86", el => el.innerText);
-  const tokenMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_totalValueWrapper__2Da_d > div.AccountInfo_totalValue__E0ehd > span:nth-child(1)",  (el) => {
-    var ele = "";
-      var myprom =new Promise(function(resolve) { 
-        setTimeout(resolve, 40000)
-    })
-      Promise.all([myprom]).then(() => {
-        console.log("waited")
-        ele = el.innerText;
-      })
-      
-    return ele
-    
-  });
-  const values = [addressMint, tokenMint]
+  const tokenMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_totalValueWrapper__2Da_d > div.AccountInfo_totalValue__E0ehd > span:nth-child(1)",  (el) => el.innerText);
+  const values = [{addressMint, tokenMint}]
   console.log(values)
   res.send({ data: values });
  
