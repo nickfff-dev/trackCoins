42854f00be10c70fdf32432b391e01b2a04594ce
unknown <hunchographics@gmail.com>
2023-02-25 23:47:01 +0300

    wh

--- dataroute.js
+++ dataroute.js
@@ -14,14 +14,20 @@ datarouter.get('/api/getmintscan', (req, res) => {
   
   const url = "https://www.mintscan.io/cosmos/account/cosmos1dy6ndu0wc5n29lfkw5gh6zpvlh2vf0u8ug8lae";
 
-  await Promise.all([page.goto(url, { waitUntil: "networkidle0"}), page.waitForRequest("https://api.mintscan.io/v1/cosmos/validators")])
-
-   
+  await page.goto(url, { waitUntil: "networkidle0", timeout: 0 })
+  var values = []
+  page.waitForSelector("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountTokens_container__1-HuH > div > div.TokenCards_container__3m0Jv > div > div > div").then( async() => {
     const addressMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_address__2WY10.AccountInfo_cursor__1Nv86", el => el.innerText);
-    const tokenMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_totalValueWrapper__2Da_d > div.AccountInfo_totalValue__E0ehd",  (el) => el.innerText);
-    const values = [{addressMint, tokenMint}]
-    console.log(values)
+    values.push(addressMint)
+    const tokenMint = await page.$eval("#__next > main > section > div > div.Account_container__pc9IN > section.Section_container__3OCWW.AccountInfo_container__1RRgK > div.AccountInfo_totalValueWrapper__2Da_d > div.AccountInfo_totalValue__E0ehd", (el) => el.innerText);
+    values.push(tokenMint)
+  })
+   
+    
+
+  if (values.length > 1) {
     res.send({ data: values });
+   }
   
 
  
